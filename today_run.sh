53899614b55d5c0e1ba9dd1d93004d9ebd46991d
woaiboge <591145292@qq.com>
2026-05-09 17:18:18 +0800

    今天基因组N库比较运行代码

---
+++ today_run.sh
@@ -0,0 +1,7 @@
+  612  touch README.md
+  626  echo 'print("hello world")' > hello.py
+  628  ls
+  629  (base) dengyj@PC-20200620MAIO:/mnt/f/bio_project$ ls
+  634  history > analysis_commands.sh
+  636  ls
+  640  rm analysis_commands.sh
